c6987afdd31ab9ae7b4e20b8e64969ac56d089db
Bruno Paschoal <programming.paschoal@gmail.com>
2026-02-14 21:55:23 -0300

    Criando listas

---
+++ Alura/Lógica de Programação Explore Funções e Listas/PARTE 04 - AULA 07/script.js
@@ -0,0 +1,15 @@
+//Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+//Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+//Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push = ['Java', 'Ruby', 'GoLang'];
+//Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+nomes = ['Bruno', 'Bruna', 'Kabeça'];
+console.log(nomes[0]);
+//Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+nomes = ['Jenna', 'Fred', 'Belinha'];
+console.log(nomes[1]);
+//Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+nomes = ['Lola', 'Pepe', 'Pandora'];
+console.log(nomes[2]);
